00f948059d1416fcb6832925d4a1f4a705242917
Nelly <wnelly08@gmail.com>
2019-07-31 13:32:01 -0400

    initial commit

---
+++ data.js
@@ -0,0 +1,110 @@
+const DATA = [
+    {
+        question: 'Which ingredient is a common and harmful sweetener 
+        found in almost all foods containing added sugar?',
+        answers: [
+            'High-fructose corn syrup',
+            'Honey',
+            'Fresh fruits',
+            'Stevia'
+        ],
+        correctAnswer: 'High-fructose corn syrup'
+    },
+    {
+        question: 'How much sugar should you eat per day?',
+        answers: [
+            '200 calories per day',
+            '100-150 calories per day',
+            '500 calories per day',
+            '1000 calories per day'
+        ],
+        correctAnswer = '100-150 calories per day'
+    },
+    {
+        question: 'Prolonged high blood glucose levels can lead to?',
+        answers: [
+            'Diabetes',
+            'Heart attack',
+            'Arrythmia',
+            'Stroke
+        ],
+        correctAnswer: "Diabetes"
+    },
+    {
+        question: 'Which is a sign of iodine deficiency?',
+        answers: [
+            'Hyperactivity',
+            'Goiter',
+            'Hair gain',
+            'Feeling hot'
+        ],
+        correctAnswer = 'Goiter'
+    },
+    {
+        question: 'Which is an excellent source of iodine?',
+        answers: [
+            'Blueberries',
+            'Seaweed',
+            'Banana',
+            'Plain rice'
+        ],
+        correctAnswer: 'Seaweed'
+    },
+    {
+        question: 'Which measurement is used to access your ideal weight?',
+        answers: [
+            'Chest circumference',
+            'Body Mass Index',
+            'Water intake',
+            'Caloric intake'
+        ],
+        correctAnswer: 'Body Mass Index'      
+    },
+    {
+        question: 'What is Alzheimer’s disease?',
+        answers: [
+            'A condition where blood vessels are narrowed or blocked leading 
+            to pain in the muscle and valves.',
+            'An irreversible brain disorder that destroys your memory and 
+            thinking skills and eventually, not able to carry out simple tasks.', 
+            'A disease that affects the blood vessels in the brain.',
+            'A disorder in the nervous system that causes tremor.'
+        ],
+        correctAnswer: 'An irreversible brain disorder that destroys your memory and 
+            thinking skills and eventually, not able to carry out simple tasks.'
+    },
+    {
+        question: 'Omega-3 fatty acids play an important role in the prevention 
+        of Alzheimer’s disease. Where are they found?',
+        answers: [
+            'Rice',
+            'Fatty fish',
+            'Carbonated drinks',
+            'Dairy'
+        ],
+        correctAnswer: 'Fatty fish'
+    },
+    {
+        question: 'Cholesterol is required for cellular production. What are the \
+        two types?',
+        answers: [
+            'CDL and BDL',
+            'HDL and LDL',
+            'GDL and BDL',
+            'None of the above'
+        ],
+        correctAnswer: 'HDL and LDL'
+    },
+    {
+        question: 'Why is it important to monitor and control your cholesterol level?',
+        answers: [
+            'High level of bad cholesterol can lead to a clot and eventually cause a 
+            heart attack of stroke.',
+            'High level of bad cholesterol can lead to insulin resistance.',
+            'Low level of cholesterol can decrease energy level.', 
+            'Low level of cholesterol can lead to ADHD.'
+        ],
+        correctAnswer: 'High level of bad cholesterol can lead to a clot and eventually cause a 
+            heart attack of stroke.'
+    }
+];
